5c47c4378f981af43347d5ba509e46317a4a0605
Caio castilho <caiodcastilho@hotmail.com>
2022-09-11 21:14:19 -0300

    Commit Do Caio 11/09

--- src/Scripts/PlayerShoot/Shot.gd
+++ src/Scripts/PlayerShoot/Shot.gd
@@ -7,10 +7,28 @@ var bullet = preload("res://src/Scenes/Bullets/Bullet-1.tscn")
 var can_fire = true
 
 func _process(_delta):
-	if Input.is_action_pressed("Fire") and can_fire:
+	if Input.is_action_pressed("FireUp") and can_fire:
 		var bullet_instance = bullet.instance()
 		bullet_instance.position = $ShootPoint.get_global_position()
 		bullet_instance.apply_impulse(Vector2(),Vector2(0,-bullet_speed))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate),"timeout")
+		can_fire = true 
+	elif Input.is_action_pressed("FireLeft") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $ShootPoint.get_global_position()
+		bullet_instance.rotation_degrees = -90
+		bullet_instance.apply_impulse(Vector2(),Vector2(-bullet_speed,0))
+		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate),"timeout")
+		can_fire = true 
+	elif Input.is_action_pressed("FireRight") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $ShootPoint.get_global_position()
+		bullet_instance.rotation_degrees = 90
+		bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0))
 		get_tree().get_root().add_child(bullet_instance)
 		can_fire = false
 		yield(get_tree().create_timer(fire_rate),"timeout")
