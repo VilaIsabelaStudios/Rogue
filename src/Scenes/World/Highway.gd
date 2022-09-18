974168215998a1304a3bc1b2c42ea122da259664
Hendrick Scheifer <hendrickfel@gmail.com>
2022-09-17 21:37:53 -0300

    Horde system

--- src/Scenes/World/Highway.gd
+++ src/Scenes/World/Highway.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
-var time = 6
+var phases=[3,5,7]
+var time = 1
 var enemy = preload("res://src/Scenes/Enemies/EnemyLv1.tscn")
+var now = 0
+var totalSpawneds = 0
 
 func _physics_process(delta):
-	if time < 6:
+	if time < 1:
 		time+=delta
 	else:
 		time = 0
-		var spawned = enemy.instance()
-		spawned.position=Vector2(450,0)
-		get_parent().call_deferred("add_child",spawned)
-		
-	
+		if totalSpawneds<phases[now]:
+			var spawned = enemy.instance()
+			spawned.position=Vector2(450,0)
+			get_parent().call_deferred("add_child",spawned)
+			totalSpawneds+=1
