c80db8b2c7089c27a49666e7cdc67b0ab13174b4
J0-M <joaomarcosavancini00@gmail.com>
2022-09-19 21:04:42 -0300

    rogue

---
+++ src/Scenes/Player/playerMove.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var vel = Vector2()
+var move_speed = 450
+
+func _physics_process(delta):
+	vel.x = 0
+	vel.y = 0
+	
+	if Input.is_action_pressed("ui_right"):
+		vel.x = vel.x + move_speed
+	if Input.is_action_pressed("ui_left"):
+		vel.x = vel.x - move_speed
+	if Input.is_action_pressed("ui_up"):
+		vel.y = vel.y - move_speed
+	if Input.is_action_pressed("ui_down"):
+		vel.y = vel.y + move_speed
+	
+	move_and_slide(vel)
